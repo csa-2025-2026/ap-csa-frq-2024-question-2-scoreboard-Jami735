7e6242a6098b6d35164cad7f4a8ccc735a6b3dc4
Jami735 <jrojasalmeda100@student.hbuhsd.edu>
2025-12-15 19:20:55 +0000

    class

--- Scoreboard.java
+++ Scoreboard.java
@@ -3,8 +3,49 @@
 # Answer close reading 1 questions here
 
 
+
 # Answer close reading 2 questions here
 
 */
 
 // Write the class below
+
+private String teamone;
+private String teamtwo;
+private int scoreone;
+private int scoretwo;
+private boolean active;
+
+public Scoreboard(String teamo, String teamt)
+{
+    teamone = teamo;
+    teamtwo =  teamt;
+    scoreone = 0;
+    scoretwo = 0;
+    active = true; 
+}
+
+public void recordPlay(int point)
+{
+    if (point == 0 )
+    {
+        if(active)
+        {
+            score += point;
+        }
+        else
+        {
+            scoretwo += point
+        }
+    }
+    else
+    {
+        active = !active;
+    }
+}
+
+public String getScore()
+{
+    String output = scoreone + " - " + scoretwo + " - " + active;
+    return output;
+}
